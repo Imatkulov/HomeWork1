5adf3e8d88ebfd7c9a5f15a882ccfb83f0a0de75
Imatkulov <rustam180492@gmail.com>
2019-05-18 22:57:04 +0600

    first commit

---
+++ Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import javax.sql.rowset.serial.SQLOutputImpl;
+
+public class Main {
+
+    public static void main(String[] args) {
+	   final String Hi;
+	   String name;
+	   Hi = "Hello";
+	   name = "World!";
+        System.out.println(Hi);
+        System.out.println(name);
+        System.out.println("Hi user");
+        System.out.println("Whats your name?");
+        System.out.println("welcome Rustam");
+
+    }
+}
